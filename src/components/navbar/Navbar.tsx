1a00bdee31163e392c8356247f9ebf28fc86a405
Gianmarco <100ferhas@gmail.com>
2022-10-04 23:08:46 +0200

    menu link color on hover

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import {BiUserCircle, FiSearch, FiShoppingCart} from "react-icons/all";
+import { BiUserCircle, FiSearch, FiShoppingCart } from "react-icons/all";
 import logo from "../../assets/images/logo.png"
 
 export default function Navbar() {
@@ -10,24 +10,24 @@ export default function Navbar() {
 
     return (
         <>
-            <nav id="navigation" className="navbar">
-                <a href="#"><FiSearch className="primary" size="1.5rem"/></a>
+            <nav className="navbar">
+                <a href="#"><FiSearch className="primary" size="1.5rem" /></a>
 
                 <a href="#" className="active">Home</a>
                 <a href="#">Shop</a>
                 <a href="#">Collections</a>
 
-                <div className="logo-container">
-                    <img src={logo} alt="logo" width={50} height={57}/>
+                <a href="#" className="logo-container">
+                    <img src={logo} alt="logo" width={50} height={57} />
                     SuperMarket
-                </div>
+                </a>
 
                 <a href="#">Services</a>
                 <a href="#">About Us</a>
                 <a href="#">Contact Us</a>
 
-                <a href="#"><FiShoppingCart className="primary" size="1.5rem"/></a>
-                <a href="#"><BiUserCircle className="primary" size="1.5rem"/></a>
+                <a href="#"><FiShoppingCart className="primary" size="1.5rem" /></a>
+                <a href="#"><BiUserCircle className="primary" size="1.5rem" /></a>
             </nav>
         </>
     );
